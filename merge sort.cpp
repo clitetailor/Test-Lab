03e5f5b9b0be9b8024023d313942777b2dbb548e
clitetailor <ducnhatphung@gmail.com>
2016-02-18 23:35:24 +0700

    day 1

---
+++ merge sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream.h>
+#include <sstream>
+using namespace std;
+
+void mergesort(int * f, int n)
+{
+	
+}
+
+int main()
+{
+	int f[argc];
+	for (int i = 1; i < argc; i++)
+	{
+		istringstream(argv[i]) >> f[i - 1];
+	}
+	
+	mergesort(f, argc - 1);
+}
